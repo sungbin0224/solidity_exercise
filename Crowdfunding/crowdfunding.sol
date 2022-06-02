04c9dcd512b9919bc4359b2c5796ff1009a0e152
sungbin0224 <74636474+sungbin0224@users.noreply.github.com>
2022-06-02 14:14:37 +0900

    Add files via upload

---
+++ Crowdfunding/crowdfunding.sol
@@ -0,0 +1,92 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract CrowdFunding{
+    mapping(address => uint) public contributors;
+    address public admin;
+    uint public noOfContributors;
+    uint public minimumContribution;
+    uint public deadline; 
+    uint public goal;
+    uint public raisedAmount;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+
+    constructor(uint _goal, uint _deadline){
+        goal = _goal;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = 100 wei;
+        admin = msg.sender;
+
+    }
+
+    function contribute() public payable {
+        require(block.timestamp < deadline, "Deadline has passed");
+        require(msg.value >= minimumContribution, "Minimum Contribution not met");
+        if(contributors[msg.sender] == 0 ) {
+            noOfContributors++;
+        }
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    receive() payable external{
+        contribute();
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getRefund() public{
+        require(block.timestamp > deadline && raisedAmount < goal);
+        require(contributors[msg.sender] != 0);
+        payable(msg.sender).transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin,"Only admin can call this function!");
+        _;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyAdmin{
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    function voteRequest(uint _requestNo) public {
+        require(contributors[msg.sender] != 0, "You must be a contributor" );
+        Request storage thisRequest = requests[_requestNo];
+
+        require(thisRequest.voters[msg.sender] == false, "Already voted");
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters++;
+    }
+
+    function makePayment(uint _requestNo) public onlyAdmin{
+        require(raisedAmount >= goal);
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed == false, "Request completed");
+        require(thisRequest.noOfVoters > noOfContributors/2);
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
+    }
+}
